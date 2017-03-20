24fe6d43964ced4b1b03525021d2b5ce418260fc
Fernando Buitrago <fbuitrago@gmail.com>
2017-03-20 20:20:52 +0000

    sending files to folders v2
    Everything is managed more optimally now, and I don't create separate new folders for galaxy images and rms images

--- sending_files_to_folders.py
+++ sending_files_to_folders.py
@@ -6,7 +6,6 @@ By Fernando Buitrago (fbuitrago@gmail.com)
 import numpy as np
 import os
 from shutil import copyfile
-from astropy.io import ascii
 from astropy.table import Table
 import pdb
 
@@ -19,12 +18,22 @@ def ids_as_str(ids):
 
 
 no_cpus = 3
-no_galaxies = len( os.listdir("./galaxy_images/") )
+catalog_file = "massive_high_z_bin.cat"
+base_path = "./high_z_galaxy_images/"
 
-gal,ra,dec = np.loadtxt("massive_high_z_bin_aegis.cat", dtype = float, unpack = True)
-ids = ids_as_str(gal)
 
-no_gals_per_directory = no_galaxies/no_cpus
+ids = os.listdir(base_path+"./galaxy_images/")
+ids[:] = [ele[:-2] for ele in ids] #I remove the .fits extension
+ids = np.array(ids, dtype = str)
+
+#tt = Table.read("massive_low_z_bin.cat", format = "ascii.commented_header", names=('ids', 'ra', 'dec', 'field'))
+#ids = ids_as_str(tt['ids'])
+#ra  = tt['ra']
+#dec = tt['dec']
+#region = tt['field']; region = np.array(region, dtype=str)
+
+no_galaxies = ids.size
+no_gals_per_directory = int(no_galaxies/no_cpus)
 
 start_value = 0
 for ii in range(1,no_cpus+1): #+1 because range upper limit otherwise is not inclusive (it does -1)
@@ -35,10 +44,10 @@ for ii in range(1,no_cpus+1): #+1 because range upper limit otherwise is not inc
     #generating the subfolders
     if not os.path.exists(cpu_folder):
         os.mkdir(cpu_folder)
-    if not os.path.exists(cpu_folder + "/galaxy_images"):
-        os.mkdir(cpu_folder + "/galaxy_images")
-    if not os.path.exists(cpu_folder + "/sigma_images"):
-        os.mkdir(cpu_folder + "/sigma_images")
+    #if not os.path.exists(cpu_folder + "/galaxy_images"):
+        #os.mkdir(cpu_folder + "/galaxy_images")
+    #if not os.path.exists(cpu_folder + "/sigma_images"):
+        #os.mkdir(cpu_folder + "/sigma_images")
     if not os.path.exists(cpu_folder + "/aux_files"):
         os.mkdir(cpu_folder + "/aux_files")
 
@@ -46,18 +55,18 @@ for ii in range(1,no_cpus+1): #+1 because range upper limit otherwise is not inc
     final_value = no_gals_per_directory*ii
     for jj in range(start_value,final_value): #+1 because range upper limit otherwise is not inclusive (it does -1)
         if ii is not no_cpus:
-            copyfile("./galaxy_images/"+ids[jj]+".fits"    ,cpu_folder+"/galaxy_images/"+ids[jj]+".fits")
-            copyfile("./sigma_images/" +ids[jj]+"_rms.fits",cpu_folder+"/sigma_images/" +ids[jj]+"_rms.fits")
+            #copyfile("./galaxy_images/"+ids[jj]+".fits"    ,cpu_folder+"/galaxy_images/"+ids[jj]+".fits")
+            #copyfile("./sigma_images/" +ids[jj]+"_rms.fits",cpu_folder+"/sigma_images/" +ids[jj]+"_rms.fits")
             if jj is final_value-1: #for the last element
-                data = Table([ids[start_value:final_value],ra[start_value:final_value],dec[start_value:final_value]], names=['gal', 'ra', 'dec'])
-                ascii.write(data,cpu_cat, Writer = ascii.CommentedHeader)
+                data = Table([ids[start_value:final_value]], names=['gal'])
+                data.write(cpu_cat, format = "ascii.commented_header")
                 start_value = final_value
         else:
             final_value = no_galaxies #because I will need to add all the restant galaxies to the final folder
-            copyfile("./galaxy_images/"+ids[jj]+".fits"    ,cpu_folder+"/galaxy_images/"+ids[jj]+".fits")
-            copyfile("./sigma_images/" +ids[jj]+"_rms.fits",cpu_folder+"/sigma_images/" +ids[jj]+"_rms.fits")
+            #copyfile("./galaxy_images/"+ids[jj]+".fits"    ,cpu_folder+"/galaxy_images/"+ids[jj]+".fits")
+            #copyfile("./sigma_images/" +ids[jj]+"_rms.fits",cpu_folder+"/sigma_images/" +ids[jj]+"_rms.fits")
             if jj is final_value-1: #for the last element 
-                data = Table([ids[start_value:final_value],ra[start_value:final_value],dec[start_value:final_value]], names=['gal', 'ra', 'dec'])
-                ascii.write(data,cpu_cat, Writer = ascii.CommentedHeader)
+                data = Table([ids[start_value:final_value]], names=['gal'])
+                data.write(cpu_cat, format = "ascii.commented_header")
                 #start_value = final_value -> This is not necessary, as it is the final folder
         
