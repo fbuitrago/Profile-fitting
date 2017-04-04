6f667691cf53d10af4c1b159df0ea241fb5a3b1b
Fernando Buitrago <fbuitrago@gmail.com>
2017-04-04 18:29:36 +0100

    small change to my_python_library
    change to ids_as_str function

--- my_python_library.py
+++ my_python_library.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def ids_as_str(ids):
-    #it returns a numpy array of strings, when receiving the ids as float numbers
+    #it returns a numpy array of strings, when receiving the ids with the .fits extension
     ids = np.array(ids, dtype=np.str)
-    ids[:] = [ele[:-2] for ele in ids]
+    ids[:] = [ele[:-5] for ele in ids]
     return ids
 
 def finding_coo_max_pix(img):
-	#it returns the x and y coordinates of the pixel with the maximum flux in a given image
+    #it returns the x and y coordinates of the pixel with the maximum flux in a given image
     return np.unravel_index(np.nanargmax(img),img.shape)
     #np.nanargmax->gets the maximum element of the image without taking into account the NaN values
     #np.unravel_index->gets the two dimensional position from a coordinate in a flatten vector
@@ -17,29 +17,29 @@ def finding_coo_max_pix(img):
 #pixels_x and pixels_y: to get the all the pixels you need to do the permutations of these two variables
 #flag_ok: tells you whether the squares were drawn within the limits of your image (0=no, 1=yes)
 def get_pix_within_square_aper(center_x,center_y,dim1,dim2,aper_size_pix):
-	#the aperture size must be divided to get the "radius", but remember it is a square
-	offset = math.floor(aper_size_pix/2.)
+    #the aperture size must be divided to get the "radius", but remember it is a square
+    offset = math.floor(aper_size_pix/2.)
 
-	x0 = center_x - offset
-	y0 = center_y - offset
-	x1 = center_x + offset
-	y1 = center_y + offset
+    x0 = center_x - offset
+    y0 = center_y - offset
+    x1 = center_x + offset
+    y1 = center_y + offset
 
-	#I get the pixels
-	pixels_x = np.arange(x0,x1)
-	pixels_y = np.arange(y0,y1)
+    #I get the pixels
+    pixels_x = np.arange(x0,x1)
+    pixels_y = np.arange(y0,y1)
 
-	#if the pixels do not clash with the image borders, flag_ok is equal to 1
-	if np.all( [pixels_x >= 0,pixels_y >= 0,pixels_x < dim1,pixels_y < dim2] ):
-		flag_ok = 1
-	else:
-		flag_ok = 0
+    #if the pixels do not clash with the image borders, flag_ok is equal to 1
+    if np.all( [pixels_x >= 0,pixels_y >= 0,pixels_x < dim1,pixels_y < dim2] ):
+        flag_ok = 1
+    else:
+        flag_ok = 0
 
-	#as I will return pixels, it makes sense they are integer numbers
-	pixels_x = pixels_x.astype(int)
-	pixels_y = pixels_y.astype(int)
+    #as I will return pixels, it makes sense they are integer numbers
+    pixels_x = pixels_x.astype(int)
+    pixels_y = pixels_y.astype(int)
 
-	return(flag_ok, pixels_x, pixels_y)
+    return(flag_ok, pixels_x, pixels_y)
 
 #resistant_mean function, a la IDL
 #OUTPUT
@@ -47,10 +47,10 @@ def get_pix_within_square_aper(center_x,center_y,dim1,dim2,aper_size_pix):
 #std_objs_after_clipping: self-descriptive
 #scatter_objs_after_clipping: self-descriptive
 def resistant_mean(vector,threshold):
-	clipping_apers              = stt.sigmaclip(vector,low=threshold,high=threshold)
-	objs_after_clipping         = clipping_apers[0] #objects after trimming
-	mean_objs_after_clipping    = np.mean(objs_after_clipping)
-	std_objs_after_clipping     = np.std(objs_after_clipping)
-	scatter_objs_after_clipping = std_objs_after_clipping*np.sqrt(objs_after_clipping.size-1)
+    clipping_apers              = stt.sigmaclip(vector,low=threshold,high=threshold)
+    objs_after_clipping         = clipping_apers[0] #objects after trimming
+    mean_objs_after_clipping    = np.mean(objs_after_clipping)
+    std_objs_after_clipping     = np.std(objs_after_clipping)
+    scatter_objs_after_clipping = std_objs_after_clipping*np.sqrt(objs_after_clipping.size-1)
 
-	return(mean_objs_after_clipping,std_objs_after_clipping,scatter_objs_after_clipping)
+    return(mean_objs_after_clipping,std_objs_after_clipping,scatter_objs_after_clipping)
